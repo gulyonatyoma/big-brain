1fee00f2bcfff69bdd1f8e563d0cf2afbf157941
Artyom Gostev <artyomgostev@MacBook-Air-Artyom.local>
2026-05-16 14:17:04 +0300

    Register PWA service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
+import AppRouter from './app/router'
 import './index.css'
-import App from './app/App'
+
+registerSW({
+  immediate: true,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>,
 )
